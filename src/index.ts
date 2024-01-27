760a90ca284a4b12b5ff666e64928ed9676725a6
natsuki <yasashisainternet@gmail.com>
2024-01-28 02:44:50 +0900

    [:+1:] : 静的ファイルの配信
    ふわっとつくりました。

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,15 @@ dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}.local`) });
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+// 静的ファイルの設定
+app.use(express.static(path.join(path.resolve(__dirname, "../public"))));
+
+app.get("/api", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(path.resolve(__dirname, "../public/index.html")));
 });
 
 app.listen(port, () => {
